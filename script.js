550d85e7709945c537826ff4e824bf7a1c1f0dce
Ismail irfan <98257637+Ismailirfan@users.noreply.github.com>
2024-05-24 12:48:33 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  const API_KEY = 'AIzaSyAAVEyh_nSUsyhpZq-N1oeb1qGNanbz_Kc';
+  const FOLDER_ID = 'https://drive.google.com/drive/folders/1_iCnZEDJpQEK524YzL3v8RWUtM9sDqge?usp=sharing'; // Replace with your public Google Drive folder ID
+
+  $('#listFiles').on('click', function() {
+    listFiles();
+  });
+
+  function listFiles() {
+    $.ajax({
+      url: `https://www.googleapis.com/drive/v3/files?q='${FOLDER_ID}'+in+parents&key=${API_KEY}`,
+      method: 'GET',
+      success: function(data) {
+        $('#fileList').empty();
+        data.files.forEach(file => {
+          $('#fileList').append(`<li>${file.name} - <a href="https://drive.google.com/file/d/${file.id}/view" target="_blank">View</a></li>`);
+        });
+      },
+      error: function(error) {
+        console.error('Error:', error);
+      }
+    });
+  }
+});
