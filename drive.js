a378339c5d7e3daa3bac13afe4ffbfd8b9b7b8a7
Ismail irfan <98257637+Ismailirfan@users.noreply.github.com>
2024-05-25 16:34:57 +0500

    Update drive.js

--- drive.js
+++ drive.js
@@ -1,38 +1,36 @@
-$(document).ready(function() {
-  const API_KEY = 'AIzaSyAAVEyh_nSUsyhpZq-N1oeb1qGNanbz_Kc';
-  const FOLDER_ID = '1_iCnZEDJpQEK524YzL3v8RWUtM9sDqge'; // Replace with your public Google Drive folder ID
+   $(document).ready(function() {
+      const API_KEY = 'AIzaSyAAVEyh_nSUsyhpZq-N1oeb1qGNanbz_Kc';
+      const FOLDER_ID = '1_iCnZEDJpQEK524YzL3v8RWUtM9sDqge'; // Replace with your public Google Drive folder ID
 
-  $('#listFiles').on('click', function() {
-    listFiles();
-  });
+      listFiles(); // Automatically fetch files when the page loads
 
-  function listFiles() {
-    $.ajax({
-      url: `https://www.googleapis.com/drive/v3/files?q='${FOLDER_ID}'+in+parents&key=${API_KEY}`,
-      method: 'GET',
-      success: function(data) {
-        $('#fileList').empty();
-        data.files.forEach(file => {
-          $('#fileList').append(`<li>${file.name} - <a href="https://drive.google.com/file/d/${file.id}/view" target="_blank">View</a></li>`);
-          
-        });
-      },
-      error: function(error) {
-        console.error('Error:', error);
-      }
-    });
-  }
-     window.fetchFileContent = function(fileId) {
+      function listFiles() {
         $.ajax({
-          url: `https://www.googleapis.com/drive/v3/files/${fileId}?alt=media&key=${API_KEY}`,
+          url: `https://www.googleapis.com/drive/v3/files?q='${FOLDER_ID}'+in+parents+and+mimeType='text/html'&key=${API_KEY}`,
           method: 'GET',
           success: function(data) {
-            $('#fileContent').html(data);
+            $('#fileList').empty();
+            data.files.forEach(function(file) {
+              fetchFileContent(file.id);
+            });
           },
           error: function(error) {
             console.error('Error:', error);
           }
         });
       }
-});
-        
+
+      window.fetchFileContent = function(fileId) {
+        $.ajax({
+          url: `https://www.googleapis.com/drive/v3/files/${fileId}?alt=media&key=${API_KEY}`,
+          method: 'GET',
+          success: function(data) {
+            $('#fileContent').html(data);
+            $('#mymodal').modal('show'); // Show the modal with fetched content
+          },
+          error: function(error) {
+            console.error('Error:', error);
+          }
+        });
+      }
+    });
